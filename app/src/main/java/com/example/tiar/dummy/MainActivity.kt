b33f606bf39da2978f46761d71c622ac242629b9
Tiago Araujo <tiar@nodesagency.com>
2018-10-11 14:34:38 +0200

    base project

---
+++ app/src/main/java/com/example/tiar/dummy/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tiar.dummy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
