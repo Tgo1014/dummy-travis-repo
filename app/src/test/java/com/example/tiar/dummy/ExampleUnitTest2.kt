e7e6fe37e9c6aa3cdda23e8955c78384544f54f9
Tiago Araujo <8679058+Tgo1014@users.noreply.github.com>
2018-10-30 15:38:09 +0100

    Feature/test (#1)
    * working

---
+++ app/src/test/java/com/example/tiar/dummy/ExampleUnitTest2.kt
@@ -0,0 +1,17 @@
+package com.example.tiar.dummy
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest2 {
+    @Test
+    fun addition_isInCorrect() {
+        assertNotEquals(4, 2 + 3)
+    }
+}
